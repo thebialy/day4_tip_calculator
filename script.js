a09c260e7f84b4116cb53a92558169883a9c273b
thebialy <67696052+thebialy@users.noreply.github.com>
2021-01-09 16:27:58 -0600

    javascript added to calc tip and check total

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-let total = document.getElementById("totalBill");
+let total = document.getElementById('btn');
+
+btn.addEventListener('click', () => {
+    const billAmount = document.getElementById('check-total')
+    const tipPercent = document.getElementById('tip-percentage').value;
+
+    const tipAmount = document.getElementById('tip-amount').value = billAmount / tipPercent;
+    document.getElementById('total-bill').value = parseFloat(billAmount) + parseFloat(tipAmount);
+})
